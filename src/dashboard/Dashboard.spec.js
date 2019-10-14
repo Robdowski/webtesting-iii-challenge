0b58cb5885cafe0b92341220b57f1d295eca4fb1
Robert <rc3331@gmail.com>
2019-10-14 14:31:19 -0500

    committing initial tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import { render } from "react-testing-library"
+import Dashboard from './Dashboard'
+
 // Test away
+
+test('App renders correctly', () =>{
+    render(<Dashboard />)
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react'
+import { render } from 'react-testing-library'
+import Display from './Display'
+
+test('Defaults to open and unlocked', () => {
+    const { getByText } = render(<Display />)
+
+    getByText(/^unlocked$/i)
+    getByText(/^open$/i)
+})
